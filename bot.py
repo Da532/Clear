8760ebfb697fb3ab9ccc59b7e24f21b50c3e12ff
Frangu Vlad <contact@kingdgrizzle.ml>
2017-10-16 17:33:45 +0300

    Improvements!

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@
 
 #---CONFIG---
 
-token = "TOKEN_HERE" #To find this, press CTRL + SHIFT + i in the Discord client revealing the inspect element prompt. Click the arrows, head over to Application, local storage and there you can find your user token :)
+token = "TOKEN_HERE" # To find this, press CTRL + SHIFT + i in the Discord client revealing the inspect element prompt. Click the arrows, head over to Application, local storage and there you can find your user token :)
+prefix = "!" # This defines the selfbot prefix. You can customize it however you like, but you should probably keep it tiny
 
 #--- BOT ---
 
@@ -12,7 +13,7 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
 
-bot = commands.Bot(command_prefix="!", self_bot=True)
+bot = commands.Bot(command_prefix=prefix, self_bot=True)
 bot.remove_command("help")
 
 @bot.event
@@ -24,8 +25,11 @@ async def on_ready():
 @bot.command(pass_context=True)
 async def clear(ctx, limit: int=None):
     async for msg in bot.logs_from(ctx.message.channel, limit=limit):
-        if msg.author:
-            await bot.delete_message(msg)
+        if msg.author.id == bot.user.id:
+            try:
+                await bot.delete_message (msg)
+            except:
+                pass
     embed = discord.Embed(description="Action completed! :smile:", color=0x00ff00)
     await bot.say (embed=embed)
 
